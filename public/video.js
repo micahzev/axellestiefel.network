98e3de391b88867fceabea064fc73aae090fb4a0
micahzev <friedland.micah@gmail.com>
2021-01-23 12:09:59 -0600

    removed the mobile showing of code, added controls to video

--- public/video.js
+++ public/video.js
@@ -1,17 +1,11 @@
-if (window.mobileAndTabletcheck()) {
-    document.getElementsByTagName('body')[0].style.backgroundColor = 'white';
-    document.getElementsByTagName('video')[0].style.display = 'none';
-    alert("I am a limited-limiting content, please watch me on a desktop browser for a full experience");
-} else {
-    var x = 0;
-    document.getElementsByTagName('video')[0].addEventListener('loadedmetadata', function() {
-        if (x == 0) {
-            var now = Math.floor((Date.now() / 1000) % 1550);
-            this.currentTime = now;
-            document.getElementsByTagName('video')[0].style.display = 'inline-block';
-            document.getElementsByTagName('pre')[0].style.display = 'none';
-            document.getElementsByTagName('body')[0].style.backgroundColor = 'black';
-            x += 1;
-        }
-    }, false);
-}
+var x = 0;
+document.getElementsByTagName('video')[0].addEventListener('loadedmetadata', function() {
+    if (x == 0) {
+        // 1540 is the number of seconds in the video
+        var now = Math.floor((Date.now() / 1000) % 1540);
+        this.currentTime = now;
+        document.getElementsByTagName('video')[0].style.display = 'inline-block';
+        document.getElementsByTagName('body')[0].style.backgroundColor = 'black';
+        x += 1;
+    }
+}, false);
